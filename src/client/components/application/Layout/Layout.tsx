b985f2285e11103dcea635ed2d9b757b32695295
nakamitsu_nozomi <exnnss0131@gmail.com>
2023-03-05 10:26:39 +0900

    Revert "feat: footerを遅延読み込み"
    This reverts commit bb8ec2d4b020791c98417ac5a8cf3b807614ba1d.

--- src/client/components/application/Layout/Layout.tsx
+++ src/client/components/application/Layout/Layout.tsx
@@ -1,7 +1,5 @@
 import type { FC, ReactNode } from 'react';
-import { useRef } from 'react';
 
-import { useIntersection } from '../../../hooks/useIntersection';
 import { Footer } from '../../navigators/Footer/Footer';
 import { Header } from '../../navigators/Header/Header';
 
@@ -11,17 +9,10 @@ type Props = {
   children: ReactNode;
 };
 
-export const Layout: FC<Props> = ({ children }) => {
-  const intersectionRef = useRef<HTMLElement>(null);
-  const intersection = useIntersection(intersectionRef, {
-    rootMargin: '300px',
-  });
-
-  return (
-    <>
-      <Header />
-      <main className={styles.container()}>{children}</main>
-      <Footer ref={intersectionRef} isIntersecting={intersection?.isIntersecting ?? false} />
-    </>
-  );
-};
+export const Layout: FC<Props> = ({ children }) => (
+  <>
+    <Header />
+    <main className={styles.container()}>{children}</main>
+    <Footer />
+  </>
+);
