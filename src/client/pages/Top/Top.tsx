ef4353f3b90c3f2a6c158a3ef52553626399f3b7
nakamitsu_nozomi <exnnss0131@gmail.com>
2023-03-12 12:34:36 +0900

    feat: resizeイベントの発火回数を一回にする

--- src/client/pages/Top/Top.tsx
+++ src/client/pages/Top/Top.tsx
@@ -1,8 +1,9 @@
 import type { FC } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Helmet } from 'react-helmet';
 
 import { Layout } from '../../components/application/Layout';
-import { ProductList } from '../../components/feature/ProductList';
+import { ProductListSlider } from '../../components/feature/ProductListSlider';
 import { ProductHeroImage } from '../../components/product/ProductHeroImage';
 import { ProductHeroImagePlaceholder } from '../../components/product/ProductHeroImage/ProductHeroImagePlaceholder';
 import { useFeatures } from '../../hooks/useFeatures';
@@ -10,13 +11,34 @@ import { useRecommendation } from '../../hooks/useRecommendation';
 
 import * as styles from './Top.styles';
 
+const breakPont = 1024;
+
 export const Top: FC = () => {
   const { recommendation } = useRecommendation();
   const { features } = useFeatures();
+  const ITEM_MIN_WIDTH = 250 as const;
+  const [isLargeWindow, setIsLargeWindow] = useState(window.innerWidth > breakPont);
+  const containerElementRef = useRef<HTMLUListElement>(null);
+  const containerWidth = containerElementRef.current?.getBoundingClientRect().width ?? 0;
+  const [visibleItemCount, setVisibleItemCount] = useState(Math.max(Math.floor(containerWidth / ITEM_MIN_WIDTH), 1));
 
+  useEffect(() => {
+    setVisibleItemCount(() => {
+      const containerWidth = containerElementRef.current?.getBoundingClientRect().width ?? 0;
+      return Math.max(Math.floor(containerWidth / ITEM_MIN_WIDTH), 1);
+    });
+    window.addEventListener('resize', () => {
+      setIsLargeWindow(window.innerWidth > breakPont);
+      setVisibleItemCount(() => {
+        const containerWidth = containerElementRef.current?.getBoundingClientRect().width ?? 0;
+        return Math.max(Math.floor(containerWidth / ITEM_MIN_WIDTH), 1);
+      });
+    });
+  }, []);
   if (recommendation === undefined || features === undefined) {
     return null;
   }
+
   return (
     <>
       <Helmet>
@@ -35,7 +57,12 @@ export const Top: FC = () => {
               return (
                 <div key={featureSection.id} className={styles.feature()}>
                   <h2 className={styles.featureHeading()}>{featureSection.title}</h2>
-                  <ProductList featureSection={featureSection} />
+                  <ProductListSlider
+                    ref={containerElementRef}
+                    featureSection={featureSection}
+                    isLargeWindow={isLargeWindow}
+                    visibleItemCount={visibleItemCount}
+                  />
                 </div>
               );
             })}
