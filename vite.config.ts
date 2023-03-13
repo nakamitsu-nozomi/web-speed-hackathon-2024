893d6ccd2a5039621c2aa536c1689eeb75f67539
nakamitsu_nozomi <exnnss0131@gmail.com>
2023-03-13 22:56:31 +0900

    feat: viteの設定を変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,9 @@
-import path from 'node:path';
-
 import preact from '@preact/preset-vite';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import viteCompression from 'vite-plugin-compression';
-import { ViteEjsPlugin } from 'vite-plugin-ejs';
-import topLevelAwait from 'vite-plugin-top-level-await';
-import wasm from 'vite-plugin-wasm';
-
-import { getFileList } from './tools/get_file_list';
-
-const publicDir = path.resolve(__dirname, './public');
-const getPublicFileList = async (targetPath: string) => {
-  const filePaths = await getFileList(targetPath);
-  const publicFiles = filePaths
-    .map((filePath) => path.relative(publicDir, filePath))
-    .map((filePath) => path.join('/', filePath));
-
-  return publicFiles;
-};
 
 export default defineConfig(async ({ mode }) => {
-  const videos = await getPublicFileList(path.resolve(publicDir, 'videos'));
-
   return {
     build: {
       assetsInlineLimit: 20480,
@@ -48,16 +29,7 @@ export default defineConfig(async ({ mode }) => {
       },
       target: 'chrome110',
     },
-    plugins: [
-      preact(),
-      wasm(),
-      topLevelAwait(),
-      ViteEjsPlugin({
-        module: '/src/client/index.tsx',
-        title: '買えるオーガニック',
-        videos,
-      }),
-    ],
+    plugins: [preact()],
     resolve: {
       alias: {
         react: 'preact-compat',
